834601d69287dd6600fcafe9830ae5153542c155
Matthew Barlocker <mbarlocker@gmail.com>
2026-05-05 12:14:45 -0600

    strict bash options

--- entry.sh
+++ entry.sh
@@ -1,42 +1,31 @@
 #!/bin/bash
-set -e
-source ~/.bashnvm
-source /env.sh
+set -Eeuo pipefail
 
-if [[ -z "${BUCKET}" ]]; then
-	echo 'No S3 bucket specified. Set BUCKET environment to start.'
-	exit 1
-fi
+APP_USER=app
+APP_GROUP=app
 
-if [[ -z "${PROFILE}" ]]; then
-	echo 'No AWS profile specified. Set PROFILE environment to start.'
-	exit 1
-fi
+source "${HOME}/.bashnvm"
+source "/env.sh"
+
+: "${BUCKET:?No S3 bucket specified. Set BUCKET environment to start.}"
+: "${PROFILE:?No AWS profile specified. Set PROFILE environment to start.}"
 
 ROOT="/app"
 MIRROR="${ROOT}/mirror"
 UPLOAD="${ROOT}/upload"
 
-if [[ ! -d "${MIRROR}" ]]; then
-	mkdir -p "${MIRROR}"
-	chown app:app "${MIRROR}"
-fi
+mkdir -p "${MIRROR}" "${UPLOAD}"
+chown "${APP_USER}:${APP_GROUP}" "${MIRROR}" "${UPLOAD}"
 
-if [[ -d "${UPLOAD}" ]]; then
-	# delete empty directories
-	find "${UPLOAD}" -mindepth 1 -type d -empty ! -delete
-	chown -R app:app "${UPLOAD}"
-else
-	mkdir -p "${UPLOAD}"
-	chown app:app "${UPLOAD}"
-fi
+find "${UPLOAD}" -mindepth 1 -type d -empty -delete
+chown -R "${APP_USER}:${APP_GROUP}" "${UPLOAD}"
 
 download() {
 	echo "syncing ${BUCKET} to ${MIRROR}"
 	aws --profile "${PROFILE}" s3 sync --delete "s3://${BUCKET}/" "${MIRROR}/"
 
 	echo "copying directories ${MIRROR} to ${UPLOAD}"
-	rsync -av -f"+ */" -f"- *" "${MIRROR}/" "${UPLOAD}/"
+	rsync -av -f '+ */' -f '- *' "${MIRROR}/" "${UPLOAD}/"
 }
 
 upload() {
@@ -51,14 +40,14 @@ upload() {
 				continue
 			fi
 
-			if [[ "${EXT}" = "${FILE}" ]]; then
+			if [[ "${EXT}" == "${FILE}" ]]; then
 				echo "Skipping file without extension ${LOCAL}"
 				continue
 			fi
 
 			echo "Minifying ${LOCAL}"
 
-			case "$(echo "${FILE}" | tr '[:upper:]' '[:lower:]')" in
+			case "${FILE,,}" in
 				*.png | *.jpeg | *.jpg | *.bmp | *.gif )
 					./node_modules/optipng-bin/cli.js -o4 "${LOCAL}"
 					;;
@@ -73,7 +62,11 @@ upload() {
 			HASH="$(md5sum "${LOCAL}" | awk '{ print $1; }')"
 			LOCAL_HASH="$(dirname "${LOCAL}")/$(basename "${LOCAL}" ".${EXT}").${HASH}.${EXT}"
 
-			aws --profile "${PROFILE}" s3 mv --cache-control 'public, max-age=31536000, immutable' "${LOCAL}" "s3://${BUCKET}/${LOCAL_HASH#*/}"
+			aws --profile "${PROFILE}" \
+				s3 mv \
+				--cache-control 'public, max-age=31536000, immutable' \
+				"${LOCAL}" \
+				"s3://${BUCKET}/${LOCAL_HASH#*/}"
 
 			FOUND=1
 		done < <(find "${UPLOAD}" -type f -print0)
@@ -87,13 +80,13 @@ upload() {
 	done
 }
 
-set -e
-
 cd "${ROOT}"
-if ! ( nvm use 2>/dev/null ); then
+
+if ! nvm use >/dev/null 2>&1; then
 	nvm install
 	nvm use
 fi
+
 corepack enable
 yarn install
 
